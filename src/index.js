509d64ff9191028fde492307a1071e772c2ecf97
Kateryna Tkachenko <kateryna.tkachenko@elsci.io>
2024-01-10 13:17:24 +0100

    Create library elsciUiEssentials

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import SnackBar from "./SnackBar/SnackBar.js";
-
 export {default as TextInput} from './TextInput/TextInput.js';
 export {default as TypeAheadInput} from './TypeAheadInput/TypeAheadInput.js';
 export {default as SelectInput} from './SelectInput/SelectInput.js';
-export {default as EditText} from './EditText/EditText.js';
+export {default as CreateEditText} from './EditText/EditText.js';
 export {default as SnackBar} from './SnackBar/SnackBar.js';
-if (window)
-    window.ui = {
-        SnackBar
-    }
