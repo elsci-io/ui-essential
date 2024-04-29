e8c9d2cbeb04b34ff9e9d8ae53ff6efef2a65fe5
SweetDealer <SweetDealer@users.noreply.github.com>
2024-04-29 16:58:17 +0000

    deploy: ab45f873a21abc13733da8a61d54ffdb3bbded5d

--- tmp_tsoutput/EditText/EditText.js
+++ tmp_tsoutput/EditText/EditText.js
@@ -97,10 +97,8 @@ export default class EditText extends HTMLElement {
         event.stopPropagation();
         if (event.target !== this.#children.popup)
             return;
-        if (this.checkValidity()) {
-            this.#children.popup.close();
-            this.#updateDisplayTextAndNotifyIfChanged();
-        }
+        this.#children.popup.close();
+        this.#updateDisplayTextAndNotifyIfChanged();
     }
     #updateDisplayTextAndNotifyIfChanged() {
         if (!this.#isValid) {
